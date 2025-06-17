3cfb27bb94d6e914f83d19f60ce0e7e9e1435b66
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-06-17 15:09:03 +0530

    Delete Messages Functionality Added

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -38,3 +38,23 @@ export const getMessages = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch messages" });
   }
 };
+
+export const deleteMessage = async (req, res) => {
+  try {
+    const message = await Message.findById(req.params.id);
+
+    if (!message) return res.status(404).json({ message: "Message not found" });
+
+    if (message.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ message: "Not authorized to delete this message" });
+    }
+
+    await message.deleteOne();
+    res.status(200).json({ message: "Message deleted successfully" });
+  } catch (err) {
+    console.error("Delete error:", err.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { getMessages, sendMessage } from "../controllers/messageController.js";
+import { deleteMessage, getMessages, sendMessage } from "../controllers/messageController.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, sendMessage).get(protect, getMessages);
+router.delete("/:id", protect, deleteMessage);
 
 export default router;
